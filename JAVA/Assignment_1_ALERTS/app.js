b49bdc5c88af12a62941ecde3c8806c38406dcfc
Muhammad Ausaf Jamal <ausaffarooqui17@gmail.com>
2023-01-26 21:25:27 +0500

    Added JAVA Folder
    3 files

---
+++ JAVA/Assignment_1_ALERTS/app.js
@@ -0,0 +1,15 @@
+// Question_1
+alert("Welcome Human")
+
+// Question_2
+alert("Error! Please enter a valid password.")
+
+// Question_3
+alert("Welcome to JS Land \n Happy Coding!")
+
+// Question_4
+alert("Welcome to JS Land")
+alert("Happy Coding!")
+
+// Question_5
+alert("")
